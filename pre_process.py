8b51fd91f47cf95937e7dc3c0efca1d1519a2216
unknown <foamliu@yeah.net>
2019-10-01 08:36:43 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -14,10 +14,10 @@ def get_data(folder):
         with open(tran_file, 'r') as file:
             lines = file.readlines()
         for i, line in enumerate(lines):
-            tokens = line.strip().split(sep=' ', maxsplit=2)
+            tokens = line.strip().split()
             audiopath = tokens[0]
             audiopath = os.path.join(folder_path, '{}.flac'.format(audiopath))
-            text = tokens[1]
+            text = ''.join(tokens[1:])
             for token in text:
                 build_vocab(token)
             samples.append('{}|{}\n'.format(audiopath, text))
@@ -25,28 +25,6 @@ def get_data(folder):
             print(text)
 
     return samples
-
-    # for i, line in enumerate(lines):
-    #     tokens = line.strip().split()
-    #     audiopath = 'data/km_kh_male/wavs/{}.wav'.format(tokens[0])
-    #     text = ''.join(tokens[1:])
-    #     for token in text:
-    #         build_vocab(token)
-    #
-    #
-    # valid_ids = random.sample(range(len(samples)), 100)
-    # train = []
-    # valid = []
-    # for id in range(len(samples)):
-    #     sample = samples[id]
-    #     if id in valid_ids:
-    #         valid.append(sample)
-    #     else:
-    #         train.append(sample)
-    #
-    # ensure_folder('filelists')
-    # print('num_train: ' + str(len(train)))
-    # print('num_valid: ' + str(len(valid)))
 
 
 def build_vocab(token):
@@ -77,3 +55,4 @@ if __name__ == "__main__":
         pickle.dump(data, file)
 
     print('vocab_size: ' + str(len(data['char2idx'])))
+    print('char2idx: ' + str(char2idx))
